570d265b89817e73e2c387399bd394dac6666e5d
Paddingtondx <42790040+xiongdawei@users.noreply.github.com>
2018-09-20 11:04:44 +0800

    Add files via upload

---
+++ Homework Sep20th, 2018 David Xiong.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 19 10:15:15 2018
+
+@author: davidxiong
+"""
+
+import turtle
+
+bob = turtle.Turtle()
+
+for i in range(1,5):
+    bob.fd(100)
+    bob.lt(90)
+    
+turtle.mainloop()
